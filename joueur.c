8ecb8bf158bb8a7cc0b9f25b0a524d2eddaaecdb
Alizée Duran <gonul_duran@hotmail.fr>
2015-11-23 14:57:35 +0100

    :cookie:

--- joueur.c
+++ joueur.c
@@ -3,6 +3,13 @@
 /* Structures de données */
 typedef struct {int etat; int contenue[10];}t_inventaire;
 typedef struct {int id; int hp; t_inventaire inventaire;}entity;
+typedef struct {int etat; int haut;int bas;int gauche;int droite; t_inventaire objets; int entite;}t_salle;
+
+/* Fonctions Joueur */
+void Joueur_initialiser() /* Initialise le joueur avec les données de départ */
+{}
+
+void Joueur_agir(t_salle labyrinthe, Joueur_Objet, Monstre_Objet_Liste) /* Cela donne toutes les possibilités de jouer pendant le tour donné */
+{}
 
 
-/*  */
